9e8ce3680e697b64f0db1d751b996daf0bde2c15
davezhao <davezhao@webank.com>
2019-03-29 17:31:20 +0800

    add words

--- scripts/add_words.py
+++ scripts/add_words.py
@@ -3,15 +3,11 @@ import os, sys
 from datetime import datetime
 from pathlib import Path
 from collections import defaultdict
-from lufly.models.tables import CharPhoneTable, CharShapeTable, WordPhoneTable
+from lufly.models.tables import db, CharPhoneTable, CharShapeTable, WordPhoneTable
 from peewee import fn
-from toolz.curried import pipe, map, filter, do, groupby, keymap
+from toolz.curried import *
 from update_word_phones import split_sy, get_double_dict, full_to_double
 from pypinyin import lazy_pinyin
-
-def exist_word_phones(word, phones):
-    num = WordPhoneTable.select().where(WordPhoneTable.word == word, WordPhoneTable.phones == phones).count()
-    return num > 0
 
 
 if __name__ == "__main__":
@@ -20,6 +16,11 @@ if __name__ == "__main__":
         sys.exit(1)
     
     _, words_path = sys.argv
+
+    exist_wordphones = pipe(WordPhoneTable.select(),
+        map(lambda e: e.word+e.phones),
+        set
+    )
 
     with open(words_path, "r", encoding='utf8') as fin:
         ft_dict = get_double_dict()
@@ -30,12 +31,16 @@ if __name__ == "__main__":
             groupby(lambda e: e),
             keymap(lambda e: (e, map(lambda e: split_sy(e), lazy_pinyin(e)))),
             map(lambda e: (e[0], ''.join(full_to_double(e[1], ft_dict)))),
-            filter(lambda e: not exist_word_phones(e[0], e[1])),
+            #filter(lambda e: not exist_word_phones(e[0], e[1])),
+            filter(lambda e: e[0]+e[1] not in exist_wordphones),
             map(lambda e: WordPhoneTable(word=e[0], phones=e[1], priority=1, updatedt=datetime.now())),
         )
 
-        for w in to_add_words:
-            print(f"add {w}")
-            w.save()
+        with db.atomic():
+            WordPhoneTable.bulk_create(to_add_words, batch_size=100)
+
+        # for w in to_add_words:
+        #     print(f"add {w}")
+        #     # w.save()
 
     print('done')
