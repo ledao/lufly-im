21233869598d8d54bba4535d241a8770bb7549ee
davezhao(赵伟伟) <davezhao@webank.com>
2019-04-04 12:57:19 +0800

    a

--- scripts/add_del_words.py
+++ scripts/add_del_words.py
@@ -1,7 +1,7 @@
 #encoding=utf8
 import os, sys
 from datetime import datetime
-from tables import db, DelWordPhoneTable
+from tables import db, DelWordTable
 from toolz.curried import map, filter, pipe, groupby, keymap
 
 
@@ -12,8 +12,8 @@ if __name__ == "__main__":
     
     _, words_path = sys.argv
 
-    exist_wordphones = pipe(DelWordPhoneTable.select(),
-        map(lambda e: e.word+e.phones),
+    exist_wordphones = pipe(DelWordTable.select(),
+        map(lambda e: e.word),
         set
     )
 
@@ -21,14 +21,13 @@ if __name__ == "__main__":
         to_add_words = pipe(fin,
             map(lambda e: e.strip()),
             filter(lambda e: e != ''),
+            filter(lambda e: e not in exist_wordphones),
             groupby(lambda e: e),
-            keymap(lambda e: e.split(" ")),
-            filter(lambda e: len(e) == 2),
-            map(lambda e: DelWordPhoneTable(word=e[0], phones=e[1], updatedt=datetime.now())),
+            keymap(lambda e: DelWordTable(word=e, updatedt=datetime.now())),
         )
 
         with db.atomic():
-            DelWordPhoneTable.bulk_create(to_add_words, batch_size=100)
+            DelWordTable.bulk_create(to_add_words, batch_size=100)
 
         # for w in to_add_words:
         #     print(f"add {w}")
