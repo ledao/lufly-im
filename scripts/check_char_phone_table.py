8b728b3e6af1609c19216e5d06ff68186a029858
davezhao(赵伟伟) <davezhao@webank.com>
2019-04-19 13:19:48 +0800

    add lu phones

--- scripts/check_char_phone_table.py
+++ scripts/check_char_phone_table.py
@@ -4,7 +4,7 @@ from toolz.curried import pipe, map, filter
 from pypinyin import lazy_pinyin
 from tables import db, CharPhoneTable
 from common import for_each, get_full, full_to_two, get_full_to_xhe_transformer, split_sy, get_full_to_zrm_transformmer
-from common import get_xhe_to_full_transformer
+from common import get_xhe_to_full_transformer, get_full_to_lu_transformmer
 
 
 def update_full(item: CharPhoneTable, full: str) -> CharPhoneTable:
@@ -19,15 +19,23 @@ def fill_zrm(item: CharPhoneTable, transformer: Dict[str, str]) -> CharPhoneTabl
     return item
 
 
+def fill_lu(item: CharPhoneTable, transformer: Dict[str, str]) -> CharPhoneTable:
+    sy = split_sy(item.full)
+    if sy[0] not in transformer or sy[1] not in transformer:
+        raise RuntimeError(f"{sy} not in transformer")
+    item.lu = transformer[sy[0]] + transformer[sy[1]]
+    return item
+
+
 if __name__ == "__main__":
 
-    null_xhe_count = CharPhoneTable.select().where(CharPhoneTable.phones == '').count()
+    null_xhe_count = CharPhoneTable.select().where(CharPhoneTable.xhe == '').count()
     if null_xhe_count != 0:
         print(f'{null_xhe_count} null xhe phones, please check manually.')
 
     null_full_count = CharPhoneTable.select().where(CharPhoneTable.full == '').count()
     if null_full_count != 0:
-        to_update_full_items = pipe(null_full_items,
+        to_update_full_items = pipe(CharPhoneTable.select().where(CharPhoneTable.full == ''),
              map(lambda e: (e, ''.join(get_full(e.char)))),
              map(lambda e: update_full(e[0], e[1])),
              )
@@ -41,7 +49,7 @@ if __name__ == "__main__":
     null_zrm_count = CharPhoneTable.select().where(CharPhoneTable.zrm == '').count()
     if null_zrm_count != 0:
         full_to_zrm_transformaer = get_full_to_zrm_transformmer()
-        to_update_zrm_items = pipe(null_zrm_items,
+        to_update_zrm_items = pipe(CharPhoneTable.select().where(CharPhoneTable.zrm == ''),
              map(lambda e: fill_zrm(e, full_to_zrm_transformaer)),
              )
         with db.atomic():
@@ -52,4 +60,17 @@ if __name__ == "__main__":
         if null_zrm_count != 0:
             print(f'{null_zrm_count} null items, please check mannually.')
 
+    null_lu_count = CharPhoneTable.select().where(CharPhoneTable.lu == '').count()
+    if null_lu_count != 0:
+        full_to_lu_transformaer = get_full_to_lu_transformmer()
+        to_update_lu_items = pipe(CharPhoneTable.select().where(CharPhoneTable.lu == ''),
+             map(lambda e: fill_lu(e, full_to_lu_transformaer)),
+             )
+        with db.atomic():
+            CharPhoneTable.bulk_update(to_update_lu_items, fields=['lu'], batch_size=100)
+        del to_update_lu_items
+        del full_to_lu_transformaer
+        null_lu_count = CharPhoneTable.select().where(CharPhoneTable.lu == '').count()
+        if null_lu_count != 0:
+            print(f'{null_lu_count} null items, please check mannually.')
     print("done")
