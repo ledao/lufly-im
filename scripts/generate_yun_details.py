3405b31c699c4eb28b071700abbc28dc0739b537
ledao <zhaowei153@126.com>
2020-04-15 19:11:32 +0800

    修复韵母生成错误

--- scripts/generate_yun_details.py
+++ scripts/generate_yun_details.py
@@ -2,65 +2,70 @@
 import os
 import sqlite3
 import json
+from typing import *
 
 if __name__ == "__main__":
     pwd = os.path.dirname(__file__)
     db = sqlite3.connect(os.path.join(pwd, "../lufly/sys_data/sys_table.sqlite"))
     cursor = db.cursor()
     q_result = cursor.execute('''
-        select 
-            a.yun, 
-            sum(a.priority) as yun_priority
-        from
-            (select 
-                char, 
-                full, 
-                priority, 
-                
+    select 
+        a.yun, 
+        sum(a.priority) as yun_priority
+    from
+        (select 
+            char, 
+            full, 
+            priority, 
+            
+            case substr(full, 1, 2) 
+            when 'sh' then 'sh' 
+            else 
                 case substr(full, 1, 2) 
-                when 'sh' then 'sh' 
+                when "ch" then "ch" 
                 else 
                     case substr(full, 1, 2) 
-                    when "ch" then "ch" 
+                    when 'zh' then 'zh' 
                     else 
-                        case substr(full, 1, 2) 
-                        when 'zh' then 'zh' 
+                        case full
+                        when 'ang' then 'a'
                         else 
-                            case full
-                            when 'ang' then 'a'
-                            else 
-                                case length(full)
-                                when 1 then full
-                                else substr(full, 1, 1)
-                                end
-                            end 
+                            case length(full)
+                            when 1 then full
+                            else substr(full, 1, 1)
+                            end
                         end 
                     end 
-                end as sheng,
-                
+                end 
+            end as sheng,
+            
+            case substr(full, 1, 2) 
+            when 'sh' then substr(full, 3, 5) 
+            else 
                 case substr(full, 1, 2) 
-                when 'sh' then substr(full, 3, 5) 
+                when 'ch' then substr(full, 3, 5) 
                 else 
                     case substr(full, 1, 2) 
-                    when 'ch' then substr(full, 3, 5) 
+                    when 'zh' then substr(full, 3, 5) 
                     else 
-                        case substr(full, 1, 2) 
-                        when 'zh' then substr(full, 3, 5) 
+                        case full
+                        when 'ang' then 'ang'
                         else 
-                            case full
-                            when 'ang' then 'ang'
+                            case length(full) 
+                            when 1 then full
                             else 
-                                case length(full) 
-                                when 1 then full
+                                case substr(full, 2, 5)
+                                when 've' then 'ue'
                                 else substr(full, 2, 5)
                                 end
-                            end 
+                            end
                         end 
-                    end
-                end as yun
-            from charphonetable)a
-        group by a.yun
-        order by yun_priority desc;
+                    end 
+                end
+            end as yun
+        from charphonetable where yun != 'r' and yun != 'n')a
+    group by a.yun
+    order by yun_priority desc;
     ''')
     ordered_yuns = []
     for e in q_result:
@@ -71,7 +76,7 @@ if __name__ == "__main__":
 
     print(ordered_yuns)
 
-    yun_infos = []
+    yun_infos: List[Dict] = []
     for yun in ordered_yuns:
         yun_info = {}
         q_result = cursor.execute(f'''
@@ -118,13 +123,17 @@ if __name__ == "__main__":
                             else 
                                 case length(full) 
                                 when 1 then full
-                                else substr(full, 2, 5)
+                                else 
+                                    case substr(full, 2, 5)
+                                    when 've' then 'ue'
+                                    else substr(full, 2, 5)
+                                    end
                                 end
                             end 
                         end 
                     end
                 end as yun                   
-                from charphonetable)a
+                from charphonetable where yun != 'r' and yun != 'n')a
         where a.yun = '{yun}'
         group by a.sheng
         order by sheng_priority desc
@@ -137,7 +146,7 @@ if __name__ == "__main__":
                 'num': sheng[1]
             } 
             sheng_infos.append(sheng_info)           
-        yun_info['index'] = 0
+        yun_info['index'] = len(yun_infos)
         yun_info['name'] = yun
         yun_info['shengs'] = sheng_infos
         yun_infos.append(yun_info)
@@ -145,8 +154,45 @@ if __name__ == "__main__":
     with open("yun_details.json", 'w', encoding='utf8') as fout:
         fout.write(json.dumps(yun_infos))
 
-
     cursor.close()
     db.close()
 
+    # yun_all_shengs = []
+    # for yun_info in yun_infos:
+    #     yun_name = yun_info['name']
+    #     if len(yun_name) == 1:
+    #         continue
+    #     yun_shengs = set([e['name'] for e in yun_info['shengs']])
+    #     yun_all_shengs.append((yun_name, yun_shengs))
+    # print(yun_all_shengs)
+    #
+    # joint_yuns = {}
+    # for i in range(len(yun_all_shengs)):
+    #     yun = yun_all_shengs[i][0]
+    #     all_shengs = yun_all_shengs[i][1]
+    #     can_jointed_yuns = []
+    #     for j in range(len(yun_all_shengs)):
+    #         if i == j:
+    #             continue
+    #         else:
+    #             other_yun = yun_all_shengs[j][0]
+    #             other_all_shengs = yun_all_shengs[j][1]
+    #             if len(all_shengs & other_all_shengs) == 0:
+    #                 can_jointed_yuns.append(other_yun)
+    #     if len(can_jointed_yuns) == 0:
+    #         continue
+    #     else:
+    #         joint_yuns[yun] = can_jointed_yuns
+    #
+    # for yun in joint_yuns:
+    #     all_joint_yuns = joint_yuns[yun]
+    #     print(yun, all_joint_yuns)
+    #     for i in range(len(all_joint_yuns)):
+    #         this_yun = all_joint_yuns[i]
+    #
+    #         if this_yun in joint_yuns:
+    #             for j in range(i + 1, len(all_joint_yuns)):
+    #                 if all_joint_yuns[j] in  joint_yuns[this_yun]:
+    #                     print(yun, this_yun, all_joint_yuns[j])
 
+    print('done')
