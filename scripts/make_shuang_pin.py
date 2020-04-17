d000fa647dd94a8b5f80cd9a2d2ddc3255ace23c
ledao <zhaowei153@126.com>
2020-04-17 18:38:07 +0800

    修复韵母生成错误

--- scripts/make_shuang_pin.py
+++ scripts/make_shuang_pin.py
@@ -11,22 +11,12 @@ import json
 import os, sys
 
 
-
-@dataclass 
+@dataclass
 class KeyDetail(object):
     name: str
-    hand: str # "L" or "R"
+    hand: str  # "L" or "R"
     from_key: str
     location: Tuple[float, float]
-
-
-@dataclass
-class Draft(object):
-    key_name: str
-    yuns: List[str]
-
-
-
 
 
 def load_keys() -> Dict[str, KeyDetail]:
@@ -51,7 +41,7 @@ def load_keys() -> Dict[str, KeyDetail]:
         'J': KeyDetail(name="J", hand="R", from_key="J", location=(6.5, 1.0)),
         'K': KeyDetail(name="K", hand="R", from_key="K", location=(7.5, 1.0)),
         'L': KeyDetail(name="L", hand="R", from_key="L", location=(8.5, 1.0)),
-        
+
         'Z': KeyDetail(name="Z", hand="L", from_key="A", location=(1.0, 2.0)),
         'X': KeyDetail(name="X", hand="L", from_key="D", location=(2.0, 2.0)),
         'C': KeyDetail(name="C", hand="L", from_key="F", location=(3.0, 2.0)),
@@ -61,46 +51,41 @@ def load_keys() -> Dict[str, KeyDetail]:
         'M': KeyDetail(name="M", hand="R", from_key="J", location=(7.0, 2.0)),
     }
 
+
 def load_yun_infos(yun_info_file: str) -> List[Dict]:
     with open(yun_info_file, 'r', encoding='utf8') as fin:
         return json.loads(fin.read())
 
+
 def load_pre_locked_yuns() -> Dict[str, str]:
     return {
         "i": "I",
+        "e": "E",
+        "u": "U",
+        "a": "A",
+        "o_uo": "O",
+        "v_ui": "V",
     }
-    return [
-        Draft(key_name="Q", sheng="q", yuns=[]),
-    ]
 
-def load_pre_locked_neighbors()-> Dict[str, List[str]]:
-    neighbors = [
-        ("uai", "king"), 
-        ("iang", "uang"), 
-        ("ue", "ve"), 
-        ("v", "ui"), 
-        ("ia", "ua"), 
+
+def load_jointed_yuns() -> List[Tuple]:
+    return [
+        ("uai", "king"),
+        ("iang", "uang"),
+        ("o", "uo"),
+        ("v", "ui"),
+        ("ia", "ua"),
         ("iong", "uong")
     ]
-    neighbor_relations = {}
-    for neighbor in neighbors:
-        for index, host in enumerate(neighbor):
-            for i in range(len(neighbor)):
-                if index == i:
-                    continue
-                else:
-                    if host in neighbor_relations:
-                        neighbor_relations[host].append(neighbor[i])
-                    else:
-                        neighbor_relations[host] = [neighbor[i]]
-    return neighbor_relations 
+
 
 def get_yun_info(yun_name: str, yun_details: List[Dict]) -> Dict:
     for detail in yun_details:
         if detail['name'] == yun_name:
             return detail
-    
+
     return None
+
 
 def get_yun_shengs_info(yun_name: str, neighbors: List[str], yun_details: List[Dict]):
     yun_info = get_yun_info(yun_name, yun_details)
@@ -113,7 +98,7 @@ def get_yun_shengs_info(yun_name: str, neighbors: List[str], yun_details: List[D
         print(f"{neighbors[neighbors_info.find(None)]} do not have shengs. It is a series error, exiting...")
         sys.exit(1)
     print(neighbors)
-    #TODO:
+    # TODO:
 
 
 def generate_drafts(yun_details: List[Dict], manual_yuns: Dict[str, str], neighbors: Dict[str, List[str]]):
@@ -127,17 +112,38 @@ def generate_drafts(yun_details: List[Dict], manual_yuns: Dict[str, str], neighb
             print(f"process yun: {yun_name}")
             if yun_name in neighbors:
                 print(f"{yun_name} has neighbor: {neighbors[yun_name]}, merge shengs info.")
-                #TODO:
-            print(f'get {yun_name} shengs info') 
+                # TODO:
+            print(f'get {yun_name} shengs info')
             shengs = get_yun_shengs_info(yun_name, [], yun_details)
-            #TODO:
+            # TODO:
             sys.exit(1)
 
+
+def get_yun_info(yun_infos: List[Dict], yun: str) -> Union[Dict, None]:
+    for info in yun_infos:
+        if yun == info['name']:
+            return info
+    return None
+
+
+def merge_jointed_yuns_info(yun_infos: List[Dict], jointed_yuns: List[Tuple[str, str]]) -> List[Dict]:
+    for yuns in jointed_yuns:
+        yun1 = yuns[0]
+        yun2 = yuns[1]
+        name = yun1 + "_" + yun2
+        yun1_info = get_yun_info(yun_infos, yun1)
+        yun2_info = get_yun_info(yun_infos, yun2)
+        index = yun1_info['index'] if yun1_info['index'] <= yun2_info['index'] else yun2_info['index']
+        raw_shengs = []
+        raw_shengs.extend(yun1_info['shengs'])
+
+    pass
 
 
 if __name__ == "__main__":
     keys = load_keys()
-    yun_details = load_yun_infos("yun_details.json")
+    yun_infos = load_yun_infos("yun_details.json")
     manual_yuns = load_pre_locked_yuns()
-    neighbors = load_pre_locked_neighbors()
-    generate_drafts(yun_details, manual_yuns, neighbors)
+    jointed_yuns = load_jointed_yuns()
+    yun_infos = merge_jointed_yuns_info(yun_infos, jointed_yuns)
+    # generate_drafts(yun_details, manual_yuns, neighbors)
