28321f736ed8f1604e5d66e3e482e810635af291
ledao <zhaowei153@126.com>
2021-11-26 13:06:37 +0800

    20211126

--- scripts/add_eng_words.py
+++ scripts/add_eng_words.py
@@ -7,27 +7,26 @@ from tables import db, EngWordTable
 from peewee import fn
 from toolz.curried import pipe, map, filter, reduceby, keymap
 
-
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print(f"USAGE: python3 {sys.argv[0]} words.txt", file=sys.stderr)
+        print("words format:english_word")
         sys.exit(1)
-    
+
     _, words_path = sys.argv
 
-    exist_wordphones = pipe(EngWordTable.select(),
-        map(lambda e: e.word),
-        set
-    )
+    exist_wordphones = pipe(EngWordTable.select(), map(lambda e: e.word), set)
 
     with open(words_path, "r", encoding='utf8') as fin:
 
-        to_add_words = pipe(fin,
+        to_add_words = pipe(
+            fin,
             map(lambda e: e.strip()),
             filter(lambda e: e != ''),
             filter(lambda e: e not in exist_wordphones),
             reduceby(lambda e: e, lambda e1, e2: e1),
-            map(lambda e: EngWordTable(word=e, priority=1, updatedt=datetime.now())),
+            map(lambda e: EngWordTable(
+                word=e, priority=1, updatedt=datetime.now())),
         )
 
         with db.atomic():
