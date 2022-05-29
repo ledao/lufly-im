af3a9d982db7b012bf3750266765915efa1296dc
davezhao <davezhao@webank.com>
2022-05-29 16:23:49 +0800

    new script

--- scripts/common.py
+++ scripts/common.py
@@ -1,12 +1,13 @@
 from collections import defaultdict
-import sys
 from typing import Tuple, List, Dict, Set
+
+from peewee import fn
+from pypinyin import lazy_pinyin
 from toolz.curried import curry, pipe, map, filter, groupby, valmap
 from toolz.curried import itemmap, valfilter
-from pypinyin import lazy_pinyin
-from tables import FullToTwoTable
+
 from tables import CharPhoneTable, CharHeShapeTable, WordPhoneTable, EngWordTable, CharLuShapeTable
-from tables import DelWordTable
+from tables import FullToTwoTable, SimplerTable
 
 
 @curry
@@ -245,53 +246,53 @@ def get_del_words() -> Set[str]:
     return set()
 
 
-def generate_one_hit_char(priority):
-    items = {
-        "去\tq": priority,
-        "我\tw": priority,
-        "二\te": priority,
-        "人\tr": priority,
-        "他\tt": priority,
-        "一\ty": priority,
-        "是\tu": priority,
-        "出\ti": priority,
-        "哦\to": priority,
-        "平\tp": priority,
-        "啊\ta": priority,
-        "三\ts": priority,
-        "的\td": priority,
-        "非\tf": priority,
-        "个\tg": priority,
-        "和\th": priority,
-        "就\tj": priority,
-        "可\tk": priority,
-        "了\tl": priority,
-        "在\tz": priority,
-        "小\tx": priority,
-        "才\tc": priority,
-        "这\tv": priority,
-        "不\tb": priority,
-        "你\tn": priority,
-        "没\tm": priority,
-    }
+def generate_one_hit_char() -> List[str]:
+    items = [
+        "去\tq",
+        "我\tw"
+        "二\te",
+        "人\tr",
+        "他\tt",
+        "一\ty",
+        "是\tu",
+        "出\ti",
+        "哦\to",
+        "平\tp",
+        "啊\ta",
+        "三\ts",
+        "的\td",
+        "非\tf",
+        "个\tg",
+        "和\th",
+        "就\tj",
+        "可\tk",
+        "了\tl",
+        "在\tz",
+        "小\tx",
+        "才\tc",
+        "这\tv",
+        "不\tb",
+        "你\tn",
+        "没\tm",
+    ]
     return items
 
 
-def generate_topest_char(char_to_phones, prioroty):
+def generate_topest_char(char_to_phones) -> List[str]:
     chars = """去 我 二 人 他  一 是 出 哦 平 啊 三 的 非 个 和 就 可 了 小 才 这 不 你 没 阿 爱 安 昂 奥 把 白 办 帮 报 被 本 蹦 比 边 表 别 滨 并 拨 部 擦 菜 参 藏 藏 草 测 岑 曾 拆 产 超 车 陈 成 吃 冲 抽 处 揣 传 窗 吹 纯 戳 次 差 从 凑 粗 窜 催 村 错 大 代 但 当 到 得 得 等 地 点 跌 定 丢 动 都 读 段 对 顿 多 额 欸 恩 嗯 而 法 反 放 费 分 风 佛 否 副 嘎 该 干 刚 高 各 给 跟 更 共 够 古 挂 怪 关 光 贵 滚 过 哈 还 含 行 好 何 黑 很 横 红 后 户 话 坏 换 黄 会 混 或 几 加 间 将 叫 接 进 经 久 据 卷 均 卡 开 看 抗 靠 克 剋 肯 坑 空 口 酷 夸 快 宽 况 亏 困 扩 拉 来 浪 老 月 乐 类 冷 里 连 两 料 列 林 另 刘 龙 楼 路 乱 论 落 率 吗 买 慢 忙 毛 么 每 们 梦 米 面 秒 灭 民 名 末 某 目 那 难 囊 闹 呢 内 嫩 能 泥 年 鸟 捏 您 宁 牛 弄 怒 暖 虐 挪 女 欧 怕 排 盘 旁 跑 配 盆 碰 批 片 票 撇 品 凭 破 剖 普 其 恰 前 强 桥 且 请 亲 穷 求 区 全 却 群 然 让 绕 热 任 仍 日 容 肉 如 软 若 撒 赛 散 扫 色 森 僧 啥 晒 山 上 少 设 深 生 时 受 帅 拴 双 水 顺 四 送 搜 苏 算 岁 所 她 太 谈 汤 套 特 疼 体 天 调 贴 听 同 头 图 团 推 托 挖 外 完 王 为 问 翁 喔 握 无 系 下 先 想 笑 些 新 熊 修 需 选 学 亚 眼 样 要 也 以 因 应 哟 用 有 与 元 云 咋 再 早 则 贼 怎 增 扎 占 长 长 找 着 真 正 只 中 周 主 抓 拽 转 装 追 桌 字 总 走 组 最 做"""
 
     exists_chars = set()
-    items = {}
+    items = []
     for char in filter(lambda e: e != "", chars.strip().split(" ")):
         if char in exists_chars:
             continue
+        exists_chars.add(char)
         if char not in char_to_phones:
             print(f"A: {char} has no phones.")
             continue
         for phone in char_to_phones[char]:
-            items[f"{char}\t{phone}"] = prioroty
+            items.append(f"{char}\t{phone}")
 
-        exists_chars.add(char)
     return items
 
 
@@ -306,8 +307,7 @@ def is_all_alpha(s: str) -> bool:
 
 def get_dd_cmds():
     cmds = [
-        '$ddcmd(<date.yyyy>年<date.m>月<date.d>日,[2011年1月1日])\torq',
-        '$ddcmd(<date.z> <time.hh>时<time.mm>分,[星期五 08时05分])\touj',
+        '$ddcmd(<date.z> <time.hh>时<time.mm>分)\touj',
         '$ddcmd(run(calc.exe),[计算器])\tojsq',
         '$ddcmd(<last.1>,★)\tz',
         '$ddcmd(run(%apppath%\\),[安装目录])\toav',
@@ -366,3 +366,106 @@ def get_dd_cmds():
         '$ddcmd(run(http://flypy.ys168.com),[小鹤网盘])\txhwp',
     ]
     return cmds
+
+
+def generate_single_chars(char_to_shape: Dict[str, List[str]]) -> List[str]:
+    result = []
+    for item in CharPhoneTable.select().order_by(
+            CharPhoneTable.priority.desc()):
+        if item.char in char_to_shape:
+            used_shapes = set()
+            for shape in char_to_shape[item.char]:
+                if shape in used_shapes:
+                    continue
+                used_shapes.add(shape)
+                result.append(f"{item.char}\t{item.xhe + shape}")
+        else:
+            print(f"drop {item} shapes")
+            result.append(f"{item.char}\t{item.xhe}")
+    return result
+
+
+def generate_simpler_words(char_to_shape: Dict[str, List[str]], char_threshold: int, word_threshold: int) -> \
+        Tuple[
+            List[str], List[str]]:
+    single_chars: Dict[str, CharPhoneTable] = {}
+    for item in CharPhoneTable.select().order_by(
+            CharPhoneTable.priority.desc()):
+        if item.char in char_to_shape:
+            used_shapes = set()
+            for shape in char_to_shape[item.char]:
+                if shape in used_shapes:
+                    continue
+                used_shapes.add(shape)
+                single_chars[item.xhe + shape] = item
+        else:
+            print(f"{item}, have no shapes")
+            single_chars[item.xhe] = item
+
+    high_pri_simpler_words = []
+    low_pri_simpler_words = []
+    exit_word_phones = set()
+    for item in WordPhoneTable.select().order_by(
+            fn.LENGTH(WordPhoneTable.word),
+            WordPhoneTable.priority.desc()):
+        if item.word + ":" + item.xhe in exit_word_phones:
+            continue
+        exit_word_phones.add(item.word + ":" + item.xhe)
+        if len(item.word) > 2:
+            continue
+        phone = item.xhe
+        if phone in single_chars:
+            if item.priority >= word_threshold and single_chars[phone].priority < char_threshold:
+                high_pri_simpler_words.append(f"{item.word}\t{phone}")
+            else:
+                low_pri_simpler_words.append(f"{item.word}\t{phone}")
+    return high_pri_simpler_words, low_pri_simpler_words
+
+
+def generate_full_words(char_to_shape: Dict[str, List[str]]) -> List[str]:
+    result = []
+    exit_word_phones = set()
+    for item in WordPhoneTable.select().order_by(
+            fn.LENGTH(WordPhoneTable.word),
+            WordPhoneTable.priority.desc()):
+        if item.word + ":" + item.xhe in exit_word_phones:
+            continue
+        exit_word_phones.add(item.word + ":" + item.xhe)
+        if item.word[0] in char_to_shape and item.word[-1] in char_to_shape:
+            used_shapes = set()
+            for shape_first in char_to_shape[item.word[0]]:
+                for shape_last in char_to_shape[item.word[-1]]:
+                    shape = shape_first[0] + ":" + shape_last[0]
+                    if shape in used_shapes:
+                        continue
+                    used_shapes.add(shape)
+                    encode = item.xhe + shape_first[0] + shape_last[0]
+                    decode = item.word
+                    result.append(f'{decode}\t{encode}')
+        else:
+            print(f"drop {item}, no shapes found")
+            continue
+
+    return result
+
+
+def generate_eng() -> List[str]:
+    result = []
+    for local_item in EngWordTable.select().where(
+            EngWordTable.priority > 0).order_by(fn.LENGTH(EngWordTable.word), EngWordTable.priority):
+        if not is_all_alpha(local_item.word):
+            continue
+        result.append(f"{local_item.word}\t{local_item.word}")
+    return result
+
+
+def generate_simpler() -> List[str]:
+    result = []
+    for item in SimplerTable.select().where(
+            SimplerTable.priority > 0).order_by(SimplerTable.priority.desc()):
+        encode = item.keys
+        decode = item.words
+        rule = f"{decode}\t{encode}"
+        result.append(f"{rule}")
+
+    return result
