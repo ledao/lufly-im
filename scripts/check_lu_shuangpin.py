d26b9f875987b987c8bf2f5ffef3941b0b8be61b
davezhao <davezhao@webank.com>
2022-06-20 09:15:54 +0800

    update 20220619

---
+++ scripts/check_lu_shuangpin.py
@@ -0,0 +1,65 @@
+from common import *
+from tables import db
+
+
+def check_lu_char(transformer: Dict[str, str]):
+    to_update_lu_items = []
+    for item in CharPhoneTable.select():
+        full = item.full
+        lu = item.lu
+        s, y = split_sy(full)
+        full_lu = transformer[s] + transformer[y]
+        if lu != full_lu:
+            to_update_lu_items.append(item)
+            item.lu = full_lu
+
+    with db.atomic():
+        CharPhoneTable.bulk_update(to_update_lu_items,
+                                   fields=['lu'],
+                                   batch_size=100)
+    null_lu_count = CharPhoneTable.select().where(
+        CharPhoneTable.lu == '').count()
+    if null_lu_count != 0:
+        print(f'{null_lu_count} null items, please check mannually.')
+
+    print(to_update_lu_items)
+    print(f'update {len(to_update_lu_items)} char items')
+
+
+def check_lu_word(transformer: Dict[str, str]):
+    to_update_lu_items = []
+    for item in WordPhoneTable.select():
+        fulls = item.full
+        lu = item.lu
+        full_lus_arr = []
+        for full in fulls.split(' '):
+            s, y = split_sy(full)
+            full_lu = transformer[s] + transformer[y]
+            full_lus_arr.append(full_lu)
+        full_lus = ''.join(full_lus_arr)
+        if lu != full_lus:
+            to_update_lu_items.append(item)
+            item.lu = full_lus
+
+    with db.atomic():
+        WordPhoneTable.bulk_update(to_update_lu_items,
+                                   fields=['lu'],
+                                   batch_size=100)
+    null_lu_count = WordPhoneTable.select().where(
+        WordPhoneTable.lu == '').count()
+    if null_lu_count != 0:
+        print(f'{null_lu_count} null items, please check mannually.')
+
+    print(to_update_lu_items)
+    print(f'update {len(to_update_lu_items)} word items')
+
+
+def main():
+    full_to_lu_transformer = get_full_to_lu_transformmer()
+    check_lu_char(full_to_lu_transformer)
+    check_lu_word(full_to_lu_transformer)
+    print("done")
+
+
+if __name__ == "__main__":
+    main()
--- scripts/check_word_phone_table.py
+++ scripts/check_word_phone_table.py
@@ -1,12 +1,9 @@
-import sys, os
-from toolz.curried import pipe, map, filter
-from tables import db, WordPhoneTable
-from common import get_full, for_each, get_full_to_xhe_transformer, full_to_two
 from common import *
+from tables import db
 
 
 def fill_full(item: WordPhoneTable) -> WordPhoneTable:
-    full = ''.join(get_full(item.word))
+    full = ' '.join(get_full(item.word))
     item.full = full
     return item
 
@@ -31,65 +28,25 @@ def fill_bingji(item: WordPhoneTable, bingji: str) -> WordPhoneTable:
     return item
 
 
-if __name__ == "__main__":
+def check_lu():
+    print('check lu')
+    full_to_lu_transformer = get_full_to_lu_transformmer()
+    to_update_lu_items = pipe(
+        WordPhoneTable.select(),
+        map(lambda e: (e, word_to_two(e.word, full_to_lu_transformer))),
+        map(lambda e: fill_lu(e[0], e[1])),
+    )
+    with db.atomic():
+        WordPhoneTable.bulk_update(to_update_lu_items,
+                                   fields=['lu'],
+                                   batch_size=100)
 
-    #print("check full")
-    #to_update_full_items = pipe(
-    #    WordPhoneTable.select().where(WordPhoneTable.full == ""),
-    #    map(lambda e: fill_full(e)),
-    #)
-    #with db.atomic():
-    #    WordPhoneTable.bulk_update(to_update_full_items,
-    #                               fields=['full'],
-    #                               batch_size=100)
-    #del to_update_full_items
 
-    #print("check xhe")
-    #full_to_xhe_transformer = get_full_to_xhe_transformer()
-    #to_update_xhe_items = pipe(
-    #    WordPhoneTable.select().where(WordPhoneTable.xhe == ""),
-    #    map(lambda e: (e, word_to_two(e.word, full_to_xhe_transformer))),
-    #    map(lambda e: fill_xhe(e[0], e[1])),
-    #)
-    #with db.atomic():
-    #    WordPhoneTable.bulk_update(to_update_xhe_items,
-    #                               fields=['xhe'],
-    #                               batch_size=100)
-    #del to_update_xhe_items
-    #del full_to_xhe_transformer
-
-    #print("check zrm")
-    #full_to_zrm_transformer = get_full_to_zrm_transformmer()
-    #to_update_zrm_items = pipe(
-    #    WordPhoneTable.select().where(WordPhoneTable.zrm == ""),
-    #    map(lambda e: (e, word_to_two(e.word, full_to_zrm_transformer))),
-    #    map(lambda e: fill_zrm(e[0], e[1])),
-    #)
-    #with db.atomic():
-    #    WordPhoneTable.bulk_update(to_update_zrm_items,
-    #                               fields=["zrm"],
-    #                               batch_size=100)
-    #del to_update_zrm_items
-    #del full_to_zrm_transformer
-
-    #print('check lu')
-    #full_to_lu_transformer = get_full_to_lu_transformmer()
-    #to_update_lu_items = pipe(
-    #    WordPhoneTable.select().where(WordPhoneTable.lu == ''),
-    #    map(lambda e: (e, word_to_two(e.word, full_to_lu_transformer))),
-    #    map(lambda e: fill_lu(e[0], e[1])),
-    #)
-    #with db.atomic():
-    #    WordPhoneTable.bulk_update(to_update_lu_items,
-    #                               fields=['lu'],
-    #                               batch_size=100)
-    #del to_update_lu_items
-    #del full_to_lu_transformer
-
+def check_bingji():
     print("check bingji")
     full_to_bingji_transformer = get_full_to_bingji_transformer()
     to_update_bingji_items = pipe(
-        WordPhoneTable.select().where(WordPhoneTable.bingji == ""),
+        WordPhoneTable.select(),
         map(lambda e:
             (e, word_to_two(e.word, full_to_bingji_transformer, bingji=True))),
         map(lambda e: fill_bingji(e[0], e[1])),
@@ -98,7 +55,48 @@ if __name__ == "__main__":
         WordPhoneTable.bulk_update(to_update_bingji_items,
                                    fields=['bingji'],
                                    batch_size=100)
-    del to_update_bingji_items
-    del full_to_bingji_transformer
+
+
+if __name__ == "__main__":
+    # print("check full")
+    # to_update_full_items = pipe(
+    #    WordPhoneTable.select().where(WordPhoneTable.full == ""),
+    #    map(lambda e: fill_full(e)),
+    # )
+    # with db.atomic():
+    #    WordPhoneTable.bulk_update(to_update_full_items,
+    #                               fields=['full'],
+    #                               batch_size=100)
+    # del to_update_full_items
+
+    # print("check xhe")
+    # full_to_xhe_transformer = get_full_to_xhe_transformer()
+    # to_update_xhe_items = pipe(
+    #    WordPhoneTable.select().where(WordPhoneTable.xhe == ""),
+    #    map(lambda e: (e, word_to_two(e.word, full_to_xhe_transformer))),
+    #    map(lambda e: fill_xhe(e[0], e[1])),
+    # )
+    # with db.atomic():
+    #    WordPhoneTable.bulk_update(to_update_xhe_items,
+    #                               fields=['xhe'],
+    #                               batch_size=100)
+    # del to_update_xhe_items
+    # del full_to_xhe_transformer
+
+    # print("check zrm")
+    # full_to_zrm_transformer = get_full_to_zrm_transformmer()
+    # to_update_zrm_items = pipe(
+    #    WordPhoneTable.select().where(WordPhoneTable.zrm == ""),
+    #    map(lambda e: (e, word_to_two(e.word, full_to_zrm_transformer))),
+    #    map(lambda e: fill_zrm(e[0], e[1])),
+    # )
+    # with db.atomic():
+    #    WordPhoneTable.bulk_update(to_update_zrm_items,
+    #                               fields=["zrm"],
+    #                               batch_size=100)
+    # del to_update_zrm_items
+    # del full_to_zrm_transformer
+
+    check_lu()
 
     print("done")
