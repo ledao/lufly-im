e85c086240d67c91f1867fde6501d4f303cc5190
davezhao <davezhao@webank.com>
2022-06-21 18:03:22 +0800

    update 20220619

--- scripts/generate_dd_xhe_phone_xhe_shape.py
+++ scripts/generate_dd_xhe_phone_xhe_shape.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
     schema = XHE_SP_SCHEMA
     generate_dd(XHE_SP_SCHEMA, output_dir)
 
-    dd_dir = 'lufly/win-dd/lufly-im-v4/$码表文件/'
+    dd_dir = 'lufly/win-dd/lufly-im-v4-xiaohe/$码表文件/'
     if os.path.exists(dd_dir):
         shutil.rmtree(dd_dir)
     shutil.copytree(output_dir, dd_dir)
