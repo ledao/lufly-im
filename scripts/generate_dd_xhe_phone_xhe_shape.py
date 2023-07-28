d91f432c7812fa9bb52a46e79813462b608986b9
davezhao <davezhao@webank.com>
2023-07-28 14:40:16 +0800

    update 20230728

--- scripts/generate_dd_xhe_phone_xhe_shape.py
+++ scripts/generate_dd_xhe_phone_xhe_shape.py
@@ -3,18 +3,20 @@ import shutil
 import sys
 from pathlib import Path
 
-from common import XHE_SP_SCHEMA
+from common import XHE_SP_SCHEMA, XHE_SHAPE_SCHAME
 from generator import generate_dd
 
 
 def main():
+    check_db = len(sys.argv) > 1 and sys.argv[1] == "check"
+
     file_name, output_dir = sys.argv[0], "dd_xiaohe_shuangpin_xiaohe_xing"
 
     if not Path(output_dir).exists():
         os.makedirs(output_dir)
 
     schema = XHE_SP_SCHEMA
-    generate_dd(XHE_SP_SCHEMA, output_dir)
+    generate_dd(XHE_SP_SCHEMA, output_dir, XHE_SHAPE_SCHAME, check_db)
 
     dd_dir = 'lufly/win-dd/lufly-im-v4-xiaohe/$码表文件/'
     if os.path.exists(dd_dir):
--- scripts/generate_dd_zrm_phone_zrm_shape.py
+++ scripts/generate_dd_zrm_phone_zrm_shape.py
@@ -3,21 +3,21 @@ import shutil
 import sys
 from pathlib import Path
 
-from generator import generate_dd
-from common import BINGJI_SP_SCHEMA, XHE_SHAPE_SCHAME
+from generator import generate_dd, ZRM_SHAPE_SCHEMA
+from common import ZRM_SP_SCHEMA
 
 
 def main():
     check_db = len(sys.argv) > 1 and sys.argv[1] == "check"
 
-    file_name, output_dir = sys.argv[0], "dd_bingji_shuangpin_xiaohe_xing"
+    fname, output_dir = sys.argv[0], "dd_ziranma_shuangpin_ziranma_xing"
 
     if not Path(output_dir).exists():
         os.makedirs(output_dir)
 
-    generate_dd(BINGJI_SP_SCHEMA, output_dir, XHE_SHAPE_SCHAME, check_db)
+    generate_dd(ZRM_SP_SCHEMA, output_dir, ZRM_SHAPE_SCHEMA, check_db)
 
-    dd_dir = 'lufly/win-dd/lufly-im-v4-bingji/$码表文件/'
+    dd_dir = 'lufly/win-dd/lufly-im-v4-ziranma-yinxing/$码表文件/'
     if os.path.exists(dd_dir):
         shutil.rmtree(dd_dir)
     shutil.copytree(output_dir, dd_dir)
