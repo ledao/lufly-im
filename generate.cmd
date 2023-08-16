863f9e580fa21c59df144f3c5c7ad99e0934e078
davezhao <davezhao@webank.com>
2023-08-16 09:29:18 +0800

    20230814

--- generate.cmd
+++ generate.cmd
@@ -10,10 +10,12 @@ python .\scripts\generate_dd_xhe_phone_zrm_shape.py
 python .\scripts\generate_dd_zrm_phone_xhe_shape.py
 python .\scripts\generate_dd_zrm_phone_zrm_shape.py
 
-powershell Compress-Archive -Force -Path ".\rime_xiaolu_shuangpin_xiaohe_xing" -DestinationPath "rime_xiaolu_shuangpin_xiaohe_xing.zip"
+set version=v2.4.0.preview1
 
-powershell Compress-Archive -Force -Path ".\rime_xiaohe_shuangpin_xiaohe_xing" -DestinationPath "rime_xiaohe_shuangpin_xiaohe_xing.zip"
-powershell Compress-Archive -Force -Path ".\rime_ziranma_shuangpin_xiaohe_xing" -DestinationPath "rime_ziranma_shuangpin_xiaohe_xing.zip"
-powershell Compress-Archive -Force -Path ".\rime_ziranma_shuangpin_ziranma_xing" -DestinationPath "rime_ziranma_shuangpin_ziranma_xing.zip"
-powershell Compress-Archive -Force -Path ".\rime_xiaohe_shuangpin_ziranma_xing" -DestinationPath "rime_xiaohe_shuangpin_ziranma_xing.zip"
+powershell Compress-Archive -Force -Path ".\rime_xiaolu_shuangpin_xiaohe_xing" -DestinationPath "rime_xiaolu_shuangpin_xiaohe_xing.%version%.zip"
+
+powershell Compress-Archive -Force -Path ".\rime_xiaohe_shuangpin_xiaohe_xing" -DestinationPath "rime_xiaohe_shuangpin_xiaohe_xing.%version%.zip"
+powershell Compress-Archive -Force -Path ".\rime_ziranma_shuangpin_xiaohe_xing" -DestinationPath "rime_ziranma_shuangpin_xiaohe_xing.%version%.zip"
+powershell Compress-Archive -Force -Path ".\rime_ziranma_shuangpin_ziranma_xing" -DestinationPath "rime_ziranma_shuangpin_ziranma_xing.%version%.zip"
+powershell Compress-Archive -Force -Path ".\rime_xiaohe_shuangpin_ziranma_xing" -DestinationPath "rime_xiaohe_shuangpin_ziranma_xing.%version%.zip"
 
