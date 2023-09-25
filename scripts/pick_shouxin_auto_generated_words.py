a6240633e583d31312e7cd2946563690487c4d53
davezhao <davezhao@webank.com>
2023-09-25 09:07:06 +0800

    update 20230925

--- scripts/pick_shouxin_auto_generated_words.py
+++ scripts/pick_shouxin_auto_generated_words.py
@@ -1,10 +1,9 @@
 import sys
-from typing import List
 
 from common import get_exists_words, meanless_word
 
 
-def load_rime_export_words(path: str, sikp_words_path: str, out_path):
+def load_shouxin_export_words(path: str, sikp_words_path: str, out_path):
     all_words = []
     exists_words = get_exists_words()
 
@@ -14,39 +13,39 @@ def load_rime_export_words(path: str, sikp_words_path: str, out_path):
             if line == '': continue
             exists_words.add(line.split(" ")[0])
 
-    with open(path, 'r', encoding='utf8') as fin:
+    with open(path, 'r', encoding='utf16') as fin:
         for line in fin:
             line = line.strip()
-            if "enc" not in line:
+            if line == '' or line.startswith("#"):
                 continue
-            if line.startswith("#"):
-                continue
+            
             cols = line.split("\t")
-            if len(cols) <= 0:
+            if len(cols) != 3:
+                print(f"error line: {line}")
                 continue
-            word = cols[1]
+
+            word = cols[0]
             if meanless_word(word):
                 continue
             if word in exists_words:
                 continue
             exists_words.add(word)
-            all_words.append(word)
+            pinyin = cols[1].replace("'", " ")
+            
+            all_words.append((word, pinyin))
 
     all_words = sorted(all_words, key=lambda e: -len(e))
-    exists_words = set()
     with open(out_path, 'w', encoding='utf8') as fout:
-        for word in all_words:
-            for i in range(len(word)):
-                this_word = word[:i + 1]
-                if this_word not in exists_words:
-                    exists_words.add(this_word)
-            fout.write(f"{word}\n")
+        for word_pinyin in all_words:
+            word = word_pinyin[0]
+            pinyin = word_pinyin[1]
+            fout.write(f"{word} {pinyin}\n")
 
 
 def main():
     filepath = sys.argv[1]
     skip_words_path = sys.argv[2]
-    load_rime_export_words(filepath, skip_words_path, "to_add_rime_words.txt")
+    load_shouxin_export_words(filepath, skip_words_path, "to_add_shouxin_words.txt")
 
     pass
 
